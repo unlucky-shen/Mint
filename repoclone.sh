f7eab1f73c1665b816eff0c75507e9684382298a
Taufiq Haikal <taufiqhaikal002@gmail.com>
2025-12-17 05:21:07 +0000

    Add script to clone configuration repositories

---
+++ repoclone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/unlucky-shen/kitty.git ~/.config/kitty
+
+git clone https://github.com/unlucky-shen/zathura.git ~/.config/zathura
+
+# Require SSH
+# git clone git@github.com:unlucky-shen/data-science-s1.git ~/Downloads
+
+# Require SSH
+# git clone git@github.com:unlucky-shen/todo-list.git ~/Downloads
